d67cd2da15873d4dd5303b301162feb0310842b8
Mohammad Afreen Aqthar <110152840+md-afreen-aqthar@users.noreply.github.com>
2024-07-09 19:18:27 +0530

    Contct_Us

--- project-2/src/App.jsx
+++ project-2/src/App.jsx
@@ -1,13 +1,18 @@
-import './App.css'
-import Navigation from './components/Navigation/Navigation';
-function App() {
- 
+import "./App.css";
+import ContactForm from "./components/ContactForm/ContactForm";
+import ContactHeader from "./components/ContactHeader/ContactHeader";
+import Navigation from "./components/Navigation/Navigation";
 
+function App() {
   return (
-   <div>
-    <Navigation/>
-   </div>
-  )
+    <div>
+      <Navigation />
+      <main className="main_container">
+        <ContactHeader />
+        <ContactForm />
+      </main>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- project-2/src/components/Navigation/Navigation.jsx
+++ project-2/src/components/Navigation/Navigation.jsx
@@ -1,7 +1,8 @@
 import styles from "./Navigation.module.css";
 const Navigation = () => {
     console.log(styles);
-    return <nav classname={`${styles.navogation} container`}>
+    return(
+    <nav classname={`${styles.navigation} container`}>
       <div classname="logo">
           <img src="/images/logo.png" alt="do some coding logo" />
       </div>
@@ -16,8 +17,8 @@ const Navigation = () => {
              Contact
           </li>
       </ul>
-    </nav>;
-      
+    </nav>
+    );
     
   }
-  export default Navigation
+  export default Navigation;
